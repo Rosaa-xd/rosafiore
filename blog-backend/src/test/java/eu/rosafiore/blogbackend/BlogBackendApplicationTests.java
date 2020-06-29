9f68402a1ff377c3d6f61c02061ec4fce233cc2f
Rosa Fiore <Rosaa-xd>
2020-06-29 21:16:57 +0200

    Spring Boot base application for the backend

---
+++ blog-backend/src/test/java/eu/rosafiore/blogbackend/BlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.rosafiore.blogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
