b97fa122c238e7bc4ed800761959ea2f8f8392a4
Rosa Fiore <Rosaa-xd>
2020-08-13 15:20:30 +0200

    Removed testing code to start working on the "real" code

--- blog-backend/src/main/java/eu/rosafiore/blogbackend/blogpost/BlogPostController.java
+++ blog-backend/src/main/java/eu/rosafiore/blogbackend/blogpost/BlogPostController.java
@@ -1,43 +1,8 @@
 package eu.rosafiore.blogbackend.blogpost;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 
 @RestController()
 public class BlogPostController {
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @PostMapping
-    public void createPost() {
-        BlogPost blogPost = new BlogPost();
-        blogPost.setEndPoint("/test");
-        blogPost.setPublishDate(new Date());
-        blogPost.setCategories(Arrays.asList("TestCategory"));
-        blogPost.setTags(Arrays.asList("TestTag"));
-        blogPost.setArticle("testPost.md");
-
-        mongoTemplate.save(blogPost, "blogposts");
-    }
-
-    @GetMapping
-    public List<BlogPost> getPost() {
-        Query query = new Query();
-        query.addCriteria(Criteria.where("endPoint").is("/test"));
-        return mongoTemplate.find(query, BlogPost.class);
-    }
 }
