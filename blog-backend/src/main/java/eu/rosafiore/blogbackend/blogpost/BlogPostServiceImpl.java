cd2e361e40662f7be1a530e27216e93269c9ceaf
Rosaa-xd <github@shadowphoenix.nl>
2020-08-19 00:02:25 +0200

    Added check for unique documents and exceptions

--- blog-backend/src/main/java/eu/rosafiore/blogbackend/blogpost/BlogPostServiceImpl.java
+++ blog-backend/src/main/java/eu/rosafiore/blogbackend/blogpost/BlogPostServiceImpl.java
@@ -1,8 +1,15 @@
 package eu.rosafiore.blogbackend.blogpost;
 
+import eu.rosafiore.blogbackend.exception.web.ResourceAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @Service
 public class BlogPostServiceImpl implements BlogPostService {
@@ -12,6 +19,18 @@ public class BlogPostServiceImpl implements BlogPostService {
 
     @Override
     public void CreateBlogPost(BlogPost blogPost) {
-        mongoTemplate.save(blogPost, "blogposts");
+        String[] endPoint = new String[] {blogPost.getEndPoint()};
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("endPoint").in(endPoint));
+
+        boolean postExistsAlready = mongoTemplate.exists(query, BlogPost.class);
+
+        if(!postExistsAlready) {
+            mongoTemplate.save(blogPost, "blogposts");
+        }
+        else {
+            throw new ResourceAlreadyExistsException("endpoint", blogPost.getEndPoint());
+        }
     }
 }
